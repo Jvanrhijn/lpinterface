548a4be387e6ff9cd19d7d778364f184f0e987e3
Jesse van Rhijn <jesse.v.rhijn@gmail.com>
2019-07-10 17:24:54 +0200

    Add .travis.yml, add gtest

---
+++ tests/test.cc
@@ -0,0 +1,11 @@
+#include "gtest/gtest.h"
+
+
+TEST(PlaceHolder, Pass) {
+  ASSERT_EQ(1, 1);
+}
+
+int main(int argc, char *argv[]) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
