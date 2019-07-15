83bb2f45f67a35fe936e5cb06c25065f9a044d22
Jesse van Rhijn <jesse.v.rhijn@gmail.com>
2019-07-15 14:39:07 +0200

    Add Gurobi cmake stuff

--- src/lpinterface_gurobi.cc
+++ src/lpinterface_gurobi.cc
@@ -2,6 +2,11 @@
 
 namespace lpint {
 
+GurobiSolver::GurobiSolver(LinearProgram&& lp) 
+  : linear_program_(&lp)
+{
+}
+
 expected<void, LpError> GurobiSolver::solve_primal() {
   return unexpected<LpError>(LpError::SolveSuccess);
 }
--- src/lpinterface_gurobi.hpp
+++ src/lpinterface_gurobi.hpp
@@ -1,22 +1,37 @@
 #ifndef LPINTERFACE_LPINTERFACE_GUROBI_H
 #define LPINTERFACE_LPINTERFACE_GUROBI_H
 
+#include <memory>
+
+#include "gurobi_c.h"
+
+#include "lp.hpp"
 #include "lpinterface.hpp"
 
+#include "data_objects.hpp"
 #include "common.hpp"
 
 namespace lpint {
 
 class GurobiSolver : public LinearProgramSolver {
  public:
-  // placeholder stuff for now
-  GurobiSolver() = default;
+  GurobiSolver(LinearProgram&& lp);
 
   ~GurobiSolver() override = default;
 
   virtual expected<void, LpError> solve_primal() override;
 
   virtual expected<void, LpError> solve_dual() override;
+
+ private:
+  //! The linear program to solve
+  std::shared_ptr<LinearProgram> linear_program_;
+
+  //! The gurobi environment object
+  std::shared_ptr<GRBenv> gurobi_env_;
+
+  //! The gurobi model object
+  std::shared_ptr<GRBmodel> gurobi_model_;
 };
 
 }  // namespace lpint
