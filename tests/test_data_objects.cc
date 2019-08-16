9fdc12156f6977ef95c5944ff5e0ff16ed482692
Jesse van Rhijn <jesse.v.rhijn@gmail.com>
2019-08-16 15:00:55 +0200

    Add test_data_objects.cc

---
+++ tests/test_data_objects.cc
@@ -0,0 +1,44 @@
+#include <vector>
+
+#include <gtest/gtest.h>
+#include <rapidcheck/gtest.h>
+
+#include "lpinterface/data_objects.hpp"
+#include "lpinterface/errors.hpp"
+
+#include "generators.hpp"
+
+using namespace lpint;
+
+RC_GTEST_PROP(DataObjects, ObjectiveThrowsIfDimensionsMismatched,
+              (std::vector<double> vs, std::vector<VarType> vts)) {
+  if (vs.size() != vts.size()) {
+    RC_ASSERT_THROWS_AS(Objective<double>(std::move(vs), std::move(vts)),
+                        MismatchedDimensionsException);
+  }
+}
+
+RC_GTEST_PROP(DataObjects, MatrixEntryThrowsIfDuplicateNonzeros, ()) {
+  const auto values = *rc::gen::container<std::vector<double>>(
+      *rc::gen::inRange(uint8_t(2), std::numeric_limits<uint8_t>::max()),
+      rc::gen::arbitrary<double>());
+  auto indices = *rc::gen::unique<std::vector<Row<double>::Index>>(
+      values.size(), rc::gen::inRange(0ul, values.size()));
+
+  const auto from = *rc::gen::inRange(0ul, values.size() / 2);
+  const auto to = *rc::gen::inRange(from + 1, values.size());
+  indices[to] = indices[from];  // make a duplicate index
+
+  RC_ASSERT_THROWS_AS(
+      MatrixEntry<double>(std::move(values), std::move(indices)),
+      InvalidMatrixEntryException);
+}
+
+RC_GTEST_PROP(DataObjects, MatrixEntryThrowsIfDimensionsMismatched,
+              (std::vector<double> vs,
+               std::vector<MatrixEntry<double>::Index> is)) {
+  if (vs.size() != is.size()) {
+    RC_ASSERT_THROWS_AS(MatrixEntry<double>(std::move(vs), std::move(is)),
+                        MismatchedDimensionsException);
+  }
+}
