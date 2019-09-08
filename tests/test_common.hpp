032b4ed786a37ae158d721783b6ca7c0376843f0
Jesse van Rhijn <jesse.v.rhijn@gmail.com>
2019-09-08 10:28:46 +0200

    Replace solve_primal and solve_dual by solve()

--- tests/test_common.hpp
+++ tests/test_common.hpp
@@ -38,7 +38,7 @@ void test_full_problem() {
 
   solver.linear_program().set_objective(Objective<double>({1, 1, 2}));
 
-  solver().set_parameter(Param::Verbosity, 0);
+  solver.set_parameter(Param::Verbosity, 0);
 
   std::vector<Constraint<double>> constr;
   constr.emplace_back(Row<double>({1, 2, 3}, {0, 1, 2}), -LPINT_INFINITY, 4.0);
