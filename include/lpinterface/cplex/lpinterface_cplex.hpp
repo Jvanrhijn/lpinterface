32211509437899340940c4e33960626ece499179
Jesse van Rhijn <jesse.v.rhijn@gmail.com>
2019-10-20 11:31:16 +0200

    Setup build configuration for cplex

--- include/lpinterface/cplex/lpinterface_cplex.hpp
+++ include/lpinterface/cplex/lpinterface_cplex.hpp
@@ -5,9 +5,24 @@
 #include "lpinterface/lpinterface.hpp"
 #include "lpinterface/cplex/lphandle_cplex.hpp"
 
+// Magic tricks to have CPLEX behave well:
+#ifndef IL_STD
+#define IL_STD
+#endif
+#include <cstring>
+#include <ilcplex/ilocplex.h>
+ILOSTLBEGIN
+// End magic tricks
+
+#include <memory> 
+
 namespace lpint {
 
 class CplexSolver : public LinearProgramSolver {
+ public:
+
+  CplexSolver(OptimizationType optim_type);
+
   bool parameter_supported(const Param param) const override;
 
   void set_parameter(const Param param, const int value) override;
@@ -23,6 +38,13 @@ class CplexSolver : public LinearProgramSolver {
   ILinearProgramHandle& linear_program() override;
 
   const Solution<double>& get_solution() const override;
+ 
+ private:
+  std::shared_ptr<IloEnv> env_;
+  std::shared_ptr<IloModel> model_;
+
+  
+ 
 };
 
 
